c3da65e52111da8dcadb96beda268661672f9923
Jasper1165 <57362269+Jasper1165@users.noreply.github.com>
2023-09-29 17:01:06 +0200

    Update Metro.js

--- classes/3dModels/Metro.js
+++ classes/3dModels/Metro.js
@@ -1,15 +1,19 @@
 import Model3D from '/classes/3dModels/Model3D';
+import * as THREE from "three";
 import {v4 as uuid} from 'uuid';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 export class Metro extends Model3D {
     #id;
     filePath = "/assets/3d/ubahn.glb";
+    animations;
+    mixer;
 
     constructor(position) {
         super();
         this.#id = uuid();
         this.position = position
+        this.mixer = new THREE.AnimationMixer();
     }
 
     // function to open doors
@@ -39,15 +43,19 @@ export class Metro extends Model3D {
         return this.#id;
     }
 
+
+
     // Overrides default renderer, because the metro needs to be shown two times
-    render(scene) {
+    render(scene,animationMixer) {
         const loader = new GLTFLoader();
+        // this.handleAnimations();
 
         loader.load(
             // resource URL
             this.filePath,
             // called when the resource is loaded
             (gltf) => { // Gebruik een arrow-functie om de juiste 'this' context te behouden
+                this.handleAnimations(gltf);
                 gltf.animations; // Array<THREE.AnimationClip>
                 gltf.scene; // THREE.Group
                 gltf.scenes; // Array<THREE.Group>
@@ -55,13 +63,40 @@ export class Metro extends Model3D {
                 gltf.asset; // Object
                 gltf.scene.position.copy(this.position);
                 scene.add(gltf.scene);
-
                 // Second metro part
                 let model2 = gltf.scene.clone();
                 model2.rotation.y = 3.1415926536; // radiants
-                model2.t
                 scene.add(model2);
             }
         );
     }
+
+    handleAnimations(model3D) {
+            const animations = model3D.animations;
+            this.mixer = new THREE.AnimationMixer(model3D.scene);
+            this.playAnimation(animations);
+    }
+
+    playAnimation(animations) {
+        console.log(animations);
+        for (var i = 0; i < animations.length; i++) {
+            var action = this.mixer.clipAction(animations[i]);
+            action.paused = false; // Hervat de animatie
+            // console.log(animations[i])
+
+            // Zorg ervoor dat de animatie in de juiste richting wordt afgespeeld (voorwaarts)
+            action.timeScale = 1;
+            action.setLoop(THREE.LoopOnce); // Bijvoorbeeld: speel de animatie slechts één keer
+            action.clampWhenFinished = true; // Houd de animatie op het laatste frame wanneer deze is voltooid
+
+            // Als de actie eindigt (completed) of als deze nog niet is gestart (uninitialized),
+            // dan start de actie opnieuw.
+            if (action._clip.tracks.length === 0 || action._clip.tracks[0].times[0] === 0) {
+                action.reset();
+            }
+            action.play();
+        }
+    }
+
+
 }
