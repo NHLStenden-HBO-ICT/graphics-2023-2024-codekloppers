1e06f3df4a9d27193f4da1e6bedb0dcdb2b58a54
Supermel4 <melvincuperus02@gmail.com>
2023-10-08 22:49:46 +0200

    Lopen en rondkijken toegevoegd

--- classes/Controllers/SceneController.js
+++ classes/Controllers/SceneController.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {User} from "../User";
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 
@@ -7,37 +8,39 @@ export class SceneController {
     camera;
     renderer;
     controls;
+    user;
 
     constructor() {
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.001, 1000);
+        this.camera = new THREE.PerspectiveCamera(110, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.rotation.set(0, Math.PI / + 2, 0);
+        this.camera.position.set(100, 1.5, 50);
+
+        this.user = new User();
         this.setRenderer();
-        this.setControls();
-        // this.setAmbientLight();
+        // this.setControls();
+        this.setAmbientLight();
     }
 
     setRenderer() {
         this.renderer = new THREE.WebGLRenderer({ antialias: true, gammaOutput: true, premultipliedAlpha: false });
-        this.renderer.setPixelRatio(window.devicePixelRatio);
+        // this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.needsUpdate=true;
-
+        // this.renderer.needsUpdate=true;
         // this.renderer.outputColorSpace = THREE.sRGBEncoding;
-        this.renderer.shadowMapType = THREE.PCFSoftShadowMap;
+        // this.renderer.shadowMapType = THREE.PCFSoftShadowMap;
         const canvas = document.getElementById('sceneCanvas');
         canvas.appendChild(this.renderer.domElement);
     }
 
     setControls() {
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.camera.position.set(0, 20, 100);
+
         this.controls.update();
     }
 
     setAmbientLight() {
         const light = new THREE.AmbientLight(0x404040, 100);
-        light.shadowMapWidth = 1024; // default is 512
-        light.shadowMapHeight = 1024; // default is 512
         this.scene.add( light );
     }
 
--- classes/User.js
+++ classes/User.js
@@ -1,11 +1,47 @@
 import * as THREE from "three";
 
-class User {
-    position;
+export class User {
+    cameraPosition;
+
+    constructor() {
+        this.cameraPosition = new THREE.Vector3();
+    }
 
     // function to walk
-    walk() {
+    walk(camera) {
         //TODO: heeft nog een body nodig
+        // Voeg een event listener toe voor lopen en draaien
+        document.addEventListener('keydown', (event) => {
+            const keyCode = event.code;
+            switch (keyCode) {
+                case 'KeyW':
+                    this.cameraPosition.z -= 0.8;
+                    break;
+                case 'KeyS':
+                    this.cameraPosition.z += 0.8;
+                    break;
+                case 'KeyA':
+                    this.cameraPosition.x -= 0.8;
+                    break;
+                case 'KeyD':
+                    this.cameraPosition.x += 0.8;
+                    break;
+                case 'KeyQ':
+                    // Roteer de camera naar links
+                    camera.rotation.y += Math.PI / 180 * 3; // Draai 3 graden
+                    break;
+                case 'KeyE':
+                    // Roteer de camera naar rechts
+                    camera.rotation.y -= Math.PI / 180 * 3; // Draai 3 graden
+                    break;
+                case 'Space':
+                    if (!isJumping) {
+                        isJumping = true;
+                        jumpVelocity = 0.2; // Begin snelheid van de sprong
+                    }
+                break;
+            }
+        });
     }
 
     turn() {
