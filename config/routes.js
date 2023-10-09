31a2ec2bfcccb4dd95cbed0d02e6c15f1e69343f
marnick-s <contact@marnick.dev>
2023-10-09 09:33:01 +0200

    shorter tunnels

--- config/routes.js
+++ config/routes.js
@@ -4,8 +4,8 @@ export class Routes {
     static routes = {
         'U5': [
             {'vector': new THREE.Vector3(0, 0, 0), 'rotation': [0, 0, 0]},
-            {'vector': new THREE.Vector3(-700, 0, 0), 'rotation': [0, 0, 0]},
-            {'vector': new THREE.Vector3(-1200, 0, 0), 'rotation': [0, 0, 0]},
+            {'vector': new THREE.Vector3(-400, 0, 0), 'rotation': [0, 0, 0]},
+            {'vector': new THREE.Vector3(-600, 0, 0), 'rotation': [0, 0, 0]},
         ],
         'U6': [
             {'vector': new THREE.Vector3(0, 0, 100), 'rotation': [0, 0, 0]},
