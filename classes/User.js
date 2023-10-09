eaf9cdaf48d69685fdb36c1ce72cd0b39d2394c7
Jasper1165 <57362269+Jasper1165@users.noreply.github.com>
2023-10-09 21:17:59 +0200

    stop user when keyup

--- classes/User.js
+++ classes/User.js
@@ -1,45 +1,47 @@
 import * as THREE from "three";
 
 export class User {
-    cameraPosition;
-    collision;
-    speed;
+    _cameraPosition;
+    _collision;
+    _speed;
+    _moveVector;
 
     constructor() {
         this.cameraPosition = new THREE.Vector3();
         this.speed = 0.8;
+        this._moveVector = new THREE.Vector3(0, 0, -1); // Initieer de bewegingsvector in de richting van de camera
     }
 
     // function to walk
     walk(camera) {
         // Voeg een event listener toe voor toetsenbordbediening
         document.addEventListener('keydown', (event) => {
+            this._moveVector = new THREE.Vector3(0, 0, -1); // Initieer de bewegingsvector in de richting van de camera
             const keyCode = event.code;
-            const moveVector = new THREE.Vector3(0, 0, -1); // Initieer de bewegingsvector in de richting van de camera
 
             // Roteer de bewegingsvector met de rotatie van de camera
-            moveVector.applyAxisAngle(new THREE.Vector3(0, 1, 0), camera.rotation.y);
+            this._moveVector.applyAxisAngle(new THREE.Vector3(0, 1, 0), camera.rotation.y);
 
             switch (keyCode) {
                 case 'KeyW':
                     // Beweeg naar voren in de richting van de camera
-                    this.cameraPosition.x += moveVector.x * this.speed;
-                    this.cameraPosition.z += moveVector.z * this.speed;
+                    this.cameraPosition.x += this._moveVector.x * this.speed;
+                    this.cameraPosition.z += this._moveVector.z * this.speed;
                     break;
                 case 'KeyS':
                     // Beweeg naar achteren in de richting tegengesteld aan de camera
-                    this.cameraPosition.x -= moveVector.x * this.speed;
-                    this.cameraPosition.z -= moveVector.z * this.speed;
+                    this.cameraPosition.x -= this._moveVector.x * this.speed;
+                    this.cameraPosition.z -= this._moveVector.z * this.speed;
                     break;
                 case 'KeyA':
                     // Beweeg naar links, onafhankelijk van de kijkrichting
-                    this.cameraPosition.x += Math.cos(camera.rotation.y) * this.speed; // Beweeg in de lokale x-richting
-                    this.cameraPosition.z += Math.sin(camera.rotation.y) * this.speed; // Beweeg in de lokale z-richting
+                    this.cameraPosition.x -= Math.cos(camera.rotation.y) * this.speed; // Beweeg in de lokale x-richting
+                    this.cameraPosition.z -= Math.sin(camera.rotation.y) * this.speed; // Beweeg in de lokale z-richting
                     break;
                 case 'KeyD':
                     // Beweeg naar rechts, onafhankelijk van de kijkrichting
-                    this.cameraPosition.x -= Math.cos(camera.rotation.y) * this.speed; // Beweeg in de lokale x-richting
-                    this.cameraPosition.z -= Math.sin(camera.rotation.y) * this.speed; // Beweeg in de lokale z-richting
+                    this.cameraPosition.x += Math.cos(camera.rotation.y) * this.speed; // Beweeg in de lokale x-richting
+                    this.cameraPosition.z += Math.sin(camera.rotation.y) * this.speed; // Beweeg in de lokale z-richting
                     break;
                 case 'KeyQ':
                     // Roteer de camera naar links
@@ -56,6 +58,22 @@ export class User {
                     }
                     break;
             }
+        });
+
+
+  /* The code snippet `document.addEventListener("keyup", (event) => { this._moveVector.set(0, 0, 0);
+  });` is adding an event listener for the "keyup" event. When a key is released, this event
+  listener sets the `_moveVector` to (0, 0, 0), effectively stopping the movement in the direction
+  of the camera. This allows the user to stop moving when they release the corresponding movement
+  key. */
+        document.addEventListener('keyup', () => {
+            // Stop de camera direct bij keyup
+            this._moveVector.set(0, 0, 0);
+
+            // Stel de exacte positie van de camera in op cameraPosition
+            this.cameraPosition.x = camera.position.x;
+            this.cameraPosition.y = camera.position.y;
+            this.cameraPosition.z = camera.position.z;
         });
     }
 
