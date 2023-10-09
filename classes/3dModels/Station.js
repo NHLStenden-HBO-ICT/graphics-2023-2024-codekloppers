82a23929ea7990911626b9b57a332dd55797043b
biebthesecond <jeltehoekstramakkum@gmail.com>
2023-10-09 21:29:16 +0200

    code formatting

--- classes/3dModels/Station.js
+++ classes/3dModels/Station.js
@@ -30,25 +30,29 @@ export class Station extends Model3D {
         (
             this.#sceneController.scene,
             new THREE.Vector3(this._position.x + 70, this._position.y + 1, this._position.z + 8),
-            new THREE.Vector3(this._position.x + -15, this._position.y - 0.041, this._position.z + 6.5), Math.PI / 2, 0
+            new THREE.Vector3(this._position.x + -15, this._position.y - 0.041, this._position.z + 6.5),
+            Math.PI / 2, 0
         )); // backwall
 
         this.#sceneController.boundingBoxes.push(this.setBoundingBox(
             this.#sceneController.scene,
             new THREE.Vector3(this._position.x + 70, this._position.y + 1, this._position.z + 8),
-            new THREE.Vector3(this._position.x + -15, this._position.y - 0.041, this._position.z - 13), Math.PI / 2, 0
+            new THREE.Vector3(this._position.x + -15, this._position.y - 0.041, this._position.z - 13),
+            Math.PI / 2, 0
         )); // backwall
 
         this.#sceneController.boundingBoxes.push(this.setBoundingBox(
             this.#sceneController.scene,
             new THREE.Vector3(this._position.x + 60, this._position.y + 1, this._position.z + 6),
-            new THREE.Vector3(this._position.x + 20, this._position.y + 0, this._position.z + 0), Math.PI / 2, Math.PI / 2)
+            new THREE.Vector3(this._position.x + 20, this._position.y + 0, this._position.z + 0),
+            Math.PI / 2, Math.PI / 2)
         ); // left border
 
         this.#sceneController.boundingBoxes.push(this.setBoundingBox(
             this.#sceneController.scene,
             new THREE.Vector3(this._position.x + 60, this._position.y + 1, this._position.z + 6),
-            new THREE.Vector3(this._position.x + -50, this._position.y + 0, this._position.z + 0), Math.PI / 2, Math.PI / 2)
+            new THREE.Vector3(this._position.x + -50, this._position.y + 0, this._position.z + 0),
+            Math.PI / 2, Math.PI / 2)
         ); // right border
 
     }
