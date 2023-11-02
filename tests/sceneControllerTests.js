9e6f9d1e4594f0897e36762a60497ace8da7f7ec
Jasper1165 <57362269+Jasper1165@users.noreply.github.com>
2023-11-02 14:49:49 +0100

    sceneController test

---
+++ tests/sceneControllerTests.js
@@ -0,0 +1,92 @@
+// collisionTests.js
+import { expect, test } from 'vitest';
+import { CheckCameraCollision } from '../classes/Actions/CheckCameraCollision';
+import * as THREE from 'three';
+import { Scene } from '../classes/Scene.js';
+
+
+
+/* test one */
+
+test('check if camera is spawned when is set to false on initial load', () => {
+  // make mock instance of camera and define properties
+  const mockCamera = {
+    position: new THREE.Vector3(1, 2, 3),
+  };
+
+  // make mock instance of scene controller and define properties
+  const mockSceneController = {
+    getCamera: () => mockCamera,
+    getScene: new THREE.Scene(),
+    boundingBoxes: [/* mock bounding boxes here */],
+    cameraSpawned: false
+  };
+
+  // check if cameraSpawned property equals to false
+  expect(mockSceneController.cameraSpawned).toEqual(false);
+});
+
+
+/* test two */
+
+test('check if boundingbox can be filled with boundingboxdata', () => {
+    // make mock instance of camera and define properties
+    const mockCamera = {
+      position: new THREE.Vector3(1, 2, 3),
+    };
+
+    // make mock instance of scene controller and define properties
+    const mockSceneController = {
+      getCamera: () => mockCamera,
+      getScene: () => new THREE.Scene(),
+      boundingBoxes: [],
+      cameraSpawned: false
+    };
+
+    // push value into boundingbox array
+    mockSceneController.boundingBoxes.push(new THREE.Vector3(0,1,2));
+
+    // check if first index in array equals to given vector
+    expect(mockSceneController.boundingBoxes[0]).toEqual(new THREE.Vector3(0,1,2));
+});
+
+
+/* test three */
+
+test('check if camera can collide with bounding box', () => {
+    // make mock instance of camera and define properties
+    const mockCamera = {
+      position: new THREE.Vector3(1, 2, 3),
+    };
+
+    const boundingBox = {
+            scene: new THREE.Scene(), // scene
+            geometry: new THREE.Vector3(11, 3, 0.5), // geometry
+            position: new THREE.Vector3(11, 4.2, 14), //  position
+            rotationX: (Math.PI / 2), // rotation on x axis
+            rotationZ: 0, // rotation on z axis
+            rotationY: - 38.40, // rotation on y axis
+            name: 'rightStair' // id
+    }
+
+    console.log(boundingBox);
+
+    // make mock instance of scene controller and define properties
+    const mockSceneController = {
+      getCamera: () => mockCamera,
+      getScene: () => new THREE.Scene(),
+      boundingBoxes: [],
+      cameraSpawned: false
+    };
+
+
+
+    // push value into boundingbox array
+    mockSceneController.boundingBoxes.push(new THREE.Vector3(0,1,2));
+
+    // check if first index in array equals to given vector
+    expect(mockSceneController.boundingBoxes[0]).toEqual(new THREE.Vector3(0,1,2));
+});
+
+
+
