76ade69d76d99c5e4b03d951f22d8a387ff52b77
marnick-s <contact@marnick.dev>
2023-11-03 10:45:40 +0100

    fix usertest

--- tests/UserTest.js
+++ tests/UserTest.js
@@ -22,28 +22,8 @@ test('User', () => {
     expect(user).toBeInstanceOf(User);
   });
 
-  // Test 2: Test voor de walk-methode
-  test('walk method should update user position correctly', () => {
-    let sceneController;
-    let user;
 
-    const setup = () => {
-      sceneController = {
-        getCamera: () => new THREE.PerspectiveCamera(),
-        getScene: () => new THREE.Scene(),
-      };
-      user = new User(sceneController);
-    };
-
-    setup();
-    user.getMoveForward() = true;
-    user.walk();
-    // Voer assertions uit om te controleren of de gebruikerspositie correct is bijgewerkt op basis van de bewegingsvlaggen
-    // Mogelijk moet je de controls-objecten en performance.now() namaken om de test te isoleren
-    // Voorbeeld: expect(mockControls.moveForward).toHaveBeenCalledWith(-expectedForwardMovement);
-  });
-
-  // Test 3: Test voor de disableWalking- en enableWalking-methoden
+  // Test 2: Test voor de disableWalking- en enableWalking-methoden
   test('disableWalking and enableWalking methods should toggle walking state', () => {
     let sceneController;
     let user;
@@ -63,7 +43,7 @@ test('User', () => {
     expect(user.walkingDisabled).toBe(false);
   });
 
-  // Test 4: Test voor de getPosition- en setPosition-methoden
+  // Test 3: Test voor de getPosition- en setPosition-methoden
   test('getPosition and setPosition methods should get and set user position', () => {
     let sceneController;
     let user;
