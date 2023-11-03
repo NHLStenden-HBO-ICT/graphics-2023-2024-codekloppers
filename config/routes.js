e9b9cb6dabb07671912cf7c1b02a7411c8e2e5bb
Jasper1165 <57362269+Jasper1165@users.noreply.github.com>
2023-11-03 16:26:35 +0100

    added name signs of stations

--- config/routes.js
+++ config/routes.js
@@ -3,14 +3,14 @@ import * as THREE from "three";
 export class Routes {
     static routes = {
         'U5': [
-            {'vector': new THREE.Vector3(0, 0, 0), 'rotation': [0, 0, 0]},
-            {'vector': new THREE.Vector3(-200, 0, 0), 'rotation': [0, 0, 0]},
-            {'vector': new THREE.Vector3(-600, 0, 0), 'rotation': [0, 0, 0]},
+            {'vector': new THREE.Vector3(0, 0, 0), 'rotation': [0, 0, 0], 'name': 'Alexanderplatz '},
+            {'vector': new THREE.Vector3(-200, 0, 0), 'rotation': [0, 0, 0], 'name': 'Rotes Rathaus'},
+            {'vector': new THREE.Vector3(-600, 0, 0), 'rotation': [0, 0, 0], 'name': 'Museumsinsel'},
         ],
         'U6': [
-            {'vector': new THREE.Vector3(0, 0, 100), 'rotation': [0, 0, 0]},
-            {'vector': new THREE.Vector3(400, 0, 100), 'rotation': [0, 0, 0]},
-            {'vector': new THREE.Vector3(800, 0, 100), 'rotation': [0, 0, 0]},
+            {'vector': new THREE.Vector3(0, 0, 100), 'rotation': [0, 0, 0], 'name': 'leeg'},
+            {'vector': new THREE.Vector3(400, 0, 100), 'rotation': [0, 0, 0], 'name': 'leeg'},
+            {'vector': new THREE.Vector3(800, 0, 100), 'rotation': [0, 0, 0], 'name': 'leeg'},
         ],
     }
 }
