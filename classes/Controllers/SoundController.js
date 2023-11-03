f470e45fd2a68bc73213cebce6082ac609a6fa9a
marnick-s <contact@marnick.dev>
2023-11-03 09:41:12 +0100

    remove loadsound

--- classes/Controllers/SoundController.js
+++ classes/Controllers/SoundController.js
@@ -9,19 +9,6 @@ export class SoundController {
         this.listener = listener;
     }
 
-    #loadSound(pathToSoundFile, duration = undefined) {
-        const sound = new THREE.Audio(this.listener);
-
-        this.#audioLoader.load(pathToSoundFile, function (buffer) {
-            sound.setBuffer(buffer);
-            sound.setLoop(false);
-            sound.duration = duration;
-            sound.setVolume(0.5);
-            sound.play();
-        });
-        return sound;
-    }
-
     loadPositionalSound(pathToSoundFile, volume = 1, refDistance = 0.5, duration = undefined) {
         const sound = new THREE.PositionalAudio(this.listener);
 
